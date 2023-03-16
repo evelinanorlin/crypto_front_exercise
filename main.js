7e6e1742cec9448ac71f5db6d4a1c355a71e45b1
Evelina Norlin <evelina.norlin@hotmail.com>
2023-03-16 17:49:03 +0100

    new repo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 let formContainer = document.getElementById('form');
+let postsContainer = document.getElementById('posts');
 localStorage.removeItem('username');
 
 renderLoginForm();
@@ -24,9 +25,9 @@ function renderLoginForm(){
       let message = document.getElementById('message');
   
       
-      let user = {"user": (username.value).toLowerCase(), "password": password.value};
+      let user = {"username": (username.value).toLowerCase(), "password": password.value};
   
-      fetch('http://localhost:3000/users/login', {
+      fetch('http://localhost:4000/api/users/login', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -36,7 +37,7 @@ function renderLoginForm(){
         .then(res => res.json())
         .then(data => {
           message.innerHTML = data.message;
-          if (data.message == "Login successfull!"){
+          if (data.message == "Logged in"){
             localStorage.setItem("username", username.value);
             renderLoggedinPage()
           }
@@ -65,9 +66,9 @@ function renderNewUserForm(){
     let newPassword = document.getElementById('newPassword');
     let newUserMessage = document.getElementById('newUserMessage');
   
-    let user = {"user": (newUsername.value).toLowerCase(), "password": newPassword.value};
+    let user = {"username": (newUsername.value).toLowerCase(), "password": newPassword.value};
   
-    fetch('http://localhost:3000/users/add', {
+    fetch('http://localhost:4000/api/users/add', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -76,7 +77,7 @@ function renderNewUserForm(){
     })
       .then(res => res.json())
       .then(data => {
-        if(data.message == "added new"){
+        if(data.message == "created user"){
           renderCreatedUser()
         } else{
           newUserMessage.innerText = 'Try another username';
@@ -99,9 +100,15 @@ function renderCreatedUser(){
 
 function renderLoggedinPage(){
   formContainer.innerHTML = `
-  <h2>Congratulations, you are now logged in!</h2>
-  <p>But there is nothing to do here...</p>
+  <h2>Congratulations, you can write a post!</h2>
+  <h3>Whats on your heart?</h3>
+  <input type="text" placeholder="write here" id="content" required>
+  <button id="postBtn">Post</button>
   <button id="logOutBtn">Log out</button>`;
+
+  let postBtn = document.getElementById('postBtn');
+
+  postBtn.addEventListener('click', newPost)
 
   let logOutBtn = document.getElementById('logOutBtn');
 
@@ -111,3 +118,45 @@ function renderLoggedinPage(){
   })
 }
 
+function newPost(){
+  let postField = document.getElementById('content');
+  let post = postField.value;
+  let author = localStorage.getItem('username');
+  let newPost = {"author": author, "blogPost": post}
+
+  fetch('http://localhost:4000/api/blogs', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPost)
+  })
+    .then(res => res.json())
+    .then(data => {
+      renderposts();
+    })
+
+}
+
+function renderposts(){
+  let postHtml = `<div><h2>Latest posts</h2>`
+
+  fetch('http://localhost:4000/api/blogs')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data[0].author)
+
+      data.map(post => {
+        postHtml += `
+        <h3>Author: ${post.author}</h3>
+        <p>${post.blogPost}</p><hr>
+      `
+      })
+
+      postHtml += `</div>`
+      console.log(postHtml);
+      postsContainer.innerHTML = postHtml; 
+    }) 
+}
+
+renderposts()
